18f40ac97788cb1b8d5dc63925383e08f887c2ed
Amcabay <84550409+Amcabay@users.noreply.github.com>
2022-05-19 07:23:35 +0700

    Update Board.h

--- Sudoku/Board.h
+++ Sudoku/Board.h
@@ -7,21 +7,31 @@ class Sudoku
 {
 private:
 	int board[9][9];
+	int change[9][9];
+	//void fillRegion(int, int);
 
-	void fillRegion(int, int);
 public:
 	Sudoku();
-
-	int randomGenerator(int);
 
 	bool cekRegion(int, int, int);
 	bool cekRow(int, int);
 	bool cekKolom(int, int);
+	bool isValid();
+	bool fillRest();
 	bool isValid(int, int, int);
+	bool findEmpty(int&, int&);
+	//bool Check_Conflicts(int, int, int);
 
 	void generateNumber();
 	void fill(int, int, int);
 	void remove(int, int);
 	void printBoard();
-};
+	void fillRegion(int, int);
+	//void Add_First_Cord();
+	//void Help_Solve(int, int);
+	//void Solve();
+	
+	int randomGenerator(int);
 
+	
+};
